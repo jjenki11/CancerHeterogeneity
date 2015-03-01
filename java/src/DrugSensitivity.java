bd781b7887be26bd2be86169d0119618764f0cd1
unknown <jjenki11@masonlive.gmu.edu>
2015-03-01 18:25:18 -0500

    Added 2 more class skeletons

---
+++ java/src/DrugSensitivity.java
@@ -0,0 +1,22 @@
+package cancer_heterogeneity;
+
+public class DrugSensitivity 
+{
+
+	/*
+	 * protocol,
+	 * SID,
+	 * CCLASS,
+	 * CCLASS2,
+	 * LAC50,
+	 * HILL,
+	 * INF,
+	 * ZERO,
+	 * MAXR,
+	 * DATA0,DATA1,DATA2,DATA3,DATA4,DATA5,DATA6,DATA7,DATA8,DATA9,DATA10,
+	 * C0,C1,C2,C3,C4,C5,C6,C7,C8,C9,C10,
+	 * name,
+	 * target,
+	 * smi
+	 */
+}
---
+++ java/src/DrugType.java
@@ -0,0 +1,14 @@
+package cancer_heterogeneity;
+
+public class DrugType 
+{
+	/*
+	 * Sample ID,
+	 * Structure,
+	 * Sample Name,
+	 * Drug Name,
+	 * NCGC Unique ID,
+	 * Primary Mechanism of Action,
+	 * Gene Symbol
+	 */
+}
